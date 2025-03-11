233a369d7eea4fe2056cd77f804856119ac271c8
donnie <xr08255920>
2025-03-11 23:44:26 +0800

    optimize start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 # !/bin/bash
 source .venv/bin/activate
 pip install -r requirements.txt
+# 杀掉已有的web_service/app.py进程
+pkill -f web_service/app.py
 nohup python web_service/app.py > info.log 2>&1 &
 echo app startup! please check info.log
 deactivate
